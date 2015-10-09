be81ad6b65347c6406b38e946bd5d0d00f520ead
Andrej Slamka <andrej.slamka@t4a.oebb.at>
2015-10-09 20:49:32 +0200

    Initial commit

---
+++ sampleapp/sampleapp-ejb/src/main/java/com/garwan/sampleapp/ejb/LoginService.java
@@ -0,0 +1,8 @@
+package com.garwan.sampleapp.ejb;
+
+import javax.ejb.Stateless;
+
+@Stateless
+public class LoginService {
+
+}
---
+++ sampleapp/sampleapp-web/src/main/java/com/garwan/jee7/sampleapp/web/App.java
@@ -0,0 +1,13 @@
+package com.garwan.jee7.sampleapp.web;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
